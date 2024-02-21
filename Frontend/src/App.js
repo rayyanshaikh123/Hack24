ac517e7ae60435eb1e29a1523b09669a18cd4fd3
rayyanshaikh123 <143872829+rayyanshaikh123@users.noreply.github.com>
2024-02-21 20:00:11 +0530

    JSX Resolved

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
     <LandPage/>
-    <div class="card" style="width: 18rem;">
+    <div class="card" style={{width: "18rem"}}>
   <img src="..." class="card-img-top" alt="..."/>
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
