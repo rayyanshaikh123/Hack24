aed82fc83724e4eedb92ae545905e1422d3ba991
rayyanshaikh123 <143872829+rayyanshaikh123@users.noreply.github.com>
2024-02-21 23:21:58 +0530

    TAilwind added6

--- Frontend/src/components/LandPage.js
+++ Frontend/src/components/LandPage.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Navbar from './Navbar'
-
+import logo from '../undraw_code_thinking_re_gka2.svg';
 export default function LandPage() {
   return (
     <div className=" bg-[url('https://images.unsplash.com/photo-1617957743097-0d20aa2ea762?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-no-repeat bg-center w-[100%] h-[100vh]">
       <Navbar />
-      <img src="c:\Users\arhan\Rayyan Development\Rayyyan React\hack24\Frontend\public\undraw_Code_thinking_re_gka2.png" alt="Product"  className='w-100 h-100' />
+      <div className="flex ">
+        <div>
+        <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste aliquid voluptatibus </h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A nobis molestiae dicta ullam repudiandae consequuntur similique fugiat quas harum facere, iste nihil excepturi in iusto velit cumque totam doloremque? Eaque.</p>
+        </div>
+        <div >
+      <img src={logo} alt="Product"  className="w-50 h-50" />
+      </div>
+      </div>
+      
      <div class="custom-shape-divider-bottom-1708533644">
        
     <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
