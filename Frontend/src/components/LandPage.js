384dbf0f9f6d9b18863778e39f01a7fab54109a4
rayyanshaikh123 <143872829+rayyanshaikh123@users.noreply.github.com>
2024-02-21 22:40:34 +0530

    TAilwind added4

--- Frontend/src/components/LandPage.js
+++ Frontend/src/components/LandPage.js
@@ -3,8 +3,15 @@ import Navbar from './Navbar'
 
 export default function LandPage() {
   return (
-    <div className=" bg-gradient-to-r from-cyan-500 to-blue-400 bg-no-repeat bg-center w-[100%] h-[100vh]">
-     <Navbar />
+    <div className=" bg-[url('https://images.unsplash.com/photo-1617957743097-0d20aa2ea762?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-no-repeat bg-center w-[100%] h-[100vh]">
+      <Navbar />
+      <img src="c:\Users\arhan\Rayyan Development\Rayyyan React\hack24\Frontend\public\undraw_Code_thinking_re_gka2.png" alt="Product"  className='w-100 h-100' />
+     <div class="custom-shape-divider-bottom-1708533644">
+       
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+    </svg>
+</div>
     </div>
   )
 }
