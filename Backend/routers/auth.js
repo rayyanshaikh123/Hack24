51a056f08802eb6f4f402edc36eb3ec2a2b59266
unknown <chiteshvarunvarun@gmail.com>
2024-02-21 15:04:50 +0530

    bc

--- Backend/routers/auth.js
+++ Backend/routers/auth.js
@@ -1,11 +1,24 @@
-const express = require("express")
+const express = require("express");
 const Router = express.Router();
-const User = require('../Models/User')
-
-Router.post('/', async(req,res)=>{
-    const user = new User(req.body);
+const User = require("../Models/User");
+const { body, validationResult } = require("express-validator");
+Router.post("/", [body("email").isEmail()], async (req, res) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
     await user.save();
-    console.log(req.body)
-    res.send(req.body);
-})
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).errors.array();
+    }
+    res.send(user);
+  } catch (error) {
+    console.log(error.message);
+    res.send(error.message);
+  }
+});
+
 module.exports = Router;
