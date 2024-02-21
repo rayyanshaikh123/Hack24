752363d34861e1e8693861d96fe33656b440cab4
unknown <chiteshvarunvarun@gmail.com>
2024-02-21 22:00:38 +0530

    user login

--- Backend/routers/auth.js
+++ Backend/routers/auth.js
@@ -5,7 +5,7 @@ var jwt = require("jsonwebtoken");
 const Router = express.Router();
 const User = require("../Models/User");
 const { body, validationResult } = require("express-validator");
-Router.post("/", [body("email").isEmail(),body("phone_no").isLength({ min: 10 }), body("password").isLength({ min: 7 })], async (req, res) => {
+Router.post("/", [body("email").isEmail(), body("phone_no").isLength({ min: 10 }), body("password").isLength({ min: 7 })], async (req, res) => {
   try {
     //validating the values first then accordingly inserting the data in db
     const errors = validationResult(req);
@@ -21,7 +21,7 @@ Router.post("/", [body("email").isEmail(),body("phone_no").isLength({ min: 10 })
       username: req.body.username,
       email: req.body.email,
       password: secPass,
-      phone_no:req.body.phone_no
+      phone_no: req.body.phone_no,
     });
     const data = {
       user: {
@@ -39,5 +39,43 @@ Router.post("/", [body("email").isEmail(),body("phone_no").isLength({ min: 10 })
     res.send(error.message);
   }
 });
+
+//authentcate user login api/auth/login
+Router.post('/login', [
+  body("email").isEmail(), 
+  body("password").isLength({ min: 7 })
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  
+  const { email, password } = req.body;
+  
+  try {
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    const passCompare = await bcrypt.compare(password, user.password);
+    if (!passCompare) {
+      return res.status(400).json({ error: "Invalid credentials" });
+    }
+
+    const data = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    const authtoken = jwt.sign(data, JWT_token);
+    res.json({ authtoken });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 
 module.exports = Router;
