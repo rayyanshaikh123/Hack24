7a1fdb5a1425e274b1bd9d93cab4c8776e6305f9
Junaid270 <junaidshkjunnu2706@gmail.com>
2024-02-21 20:59:07 +0530

    nothng

--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -43,11 +43,3 @@ export default function Navbar() {
   )
 }
 
-<div class="card" style="width: 18rem;">
-<img src="..." class="card-img-top" alt="..."/>
-<div class="card-body">
-  <h5 class="card-title">Card title</h5>
-  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  <a href="c:\Users\gadre\Hack24\Frontend\public\index.jpg" class="btn btn-primary">Go somewhere</a>
-</div>
-</div>
